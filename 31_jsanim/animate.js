8b020d4b0f043de4f4a3a8a21f614aebd8a03d0a
Jacob Guo <78173100+jguo30@users.noreply.github.com>
2023-04-25 23:13:17 -0400

    Update animate.js
    boom

--- 31_jsanim/animate.js
+++ 31_jsanim/animate.js
@@ -16,29 +16,32 @@ var radius = 100;
 var growing = true;
 
 var drawDot = (e) =>{
-    clear(e);
+    window.cancelAnimationFrame(requestID);
+    
+    clear();
     if(radius === 250) {growing = false}
     if(radius === 0) {growing = true}
-    // while(true){
-        if (growing){
-            radius += 5;
-        }
-        else{
-            radius -= 5;
-        }
-        console.log(radius);
-        ctx.beginPath();
-        ctx.arc(c.width / 2, c.height / 2, radius, 0, Math.PI * 2);
-        ctx.stroke();
-        ctx.fill();
-    // }
+    if (growing){
+         radius += 5;
+     }
+     else{
+         radius -= 5;
+     }
+     console.log(radius);
+     ctx.beginPath();
+     ctx.arc(c.width / 2, c.height / 2, radius, 0, Math.PI * 2);
+     ctx.stroke();
+     ctx.fill();
+    
+    requestID = window.requestAnimationFrame(drawDot);
     
 }
 
 var stopIt = (e) => {
     console.log("stopit invoked...");
     console.long( requestID );
-
+    
+    window.cancelAnimationFrame(requestID);
 }
 
 
