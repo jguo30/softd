e2175b3e64051f84fa2da3a412e684f3800f859a
Jacob Guo <jguo30@stuy.edu>
2023-04-27 14:04:51 -0400

    super boom

--- 33_jsprop/3/events.js
+++ 33_jsprop/3/events.js
@@ -8,7 +8,7 @@ var table = document.getElementsByTagName('table')[0];
 var clicky = function(e) {
   alert( this.innerHTML );
   //Q: What will happen when next line is uncommented?
-  //e.stopPropagation();
+  e.stopPropagation();
 };
 
 for (var x=0; x < tds.length; x++) {
@@ -24,6 +24,6 @@ for (x=0; x < trs.length; x++) {
 //   (Leave exactly 1 version uncommented to test...)
 
 table.addEventListener('click', clicky, true);
-//table.addEventListener('click', clicky, false);
+// table.addEventListener('click', clicky, false);
 
 // Q: When user clicks on a cell, in what order will the pop-ups appear?
--- 33_jsprop/4/events.js
+++ 33_jsprop/4/events.js
@@ -20,15 +20,18 @@ var clicky = function(e) {
 //Q: What effect does the boolean arg have in each?
 //   (Leave exactly 1 version uncommented to test...)
 
+table.addEventListener('click', clicky, true);
+// table.addEventListener('click', clicky, false);
+
 for (var x=0; x < tds.length; x++) {
-  tds[x].addEventListener('click', clicky, true);
-  //tds[x].addEventListener('click', clicky, false);
+  // tds[x].addEventListener('click', clicky, true);
+  tds[x].addEventListener('click', clicky, false);
 }
 
 for (x=0; x < trs.length; x++) {
   trs[x].addEventListener('click', clicky, true);
-  //trs[x].addEventListener('click', clicky, false);
+  // trs[x].addEventListener('click', clicky, false);
 }
 
-table.addEventListener('click', clicky, true);
-//table.addEventListener('click', clicky, false);
+// table.addEventListener('click', clicky, true);
+// // table.addEventListener('click', clicky, false);
